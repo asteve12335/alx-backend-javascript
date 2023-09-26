1d9dbd14c1ce565d237060c242d6dbfb8a46d5a6
@steve <93563603+asteve12335@users.noreply.github.com>
2023-09-26 14:28:30 +0100

    Object property value shorthand syntax

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,9 +1,9 @@
 export default function getBudgetObject(income, gdp, capita) {
-  const budget = new Map([
-    ["income": income],
-    ["gdp": gdp],
-    ["capita": capita]
-  ]);
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
 
   return budget;
 }
